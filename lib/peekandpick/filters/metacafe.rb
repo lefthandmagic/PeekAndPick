8fbf0eab10dbedbf8546eccebf7ecbed2d11e45e
Praveen Murugesan <praveen@praveen-laptop.(none)>
2012-01-07 14:09:47 -0800

    minor fixes

--- lib/peekandpick/filters/metacafe.rb
+++ lib/peekandpick/filters/metacafe.rb
@@ -21,7 +21,7 @@ metacafe_lambda = lambda { |text, options|
         result['description'] = link.attributes['content'].to_s
       end
     end
-    result['value'] = %{<div style="background:#000000;width:#{width}px;height:#{height}px"><embed flashVars="playerVars=#{flash_vars}" src="http://www.metacafe.com/fplayer/#{metacafe_id}/#{metacafe_slug}.swf" width="#{width}" height="#{height}" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_#{metacafe_id}" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed></div>}
+    result['value'] = %{<embed flashVars="playerVars=#{flash_vars}" src="http://www.metacafe.com/fplayer/#{metacafe_id}/#{metacafe_slug}.swf" width="#{width}" height="#{height}" wmode="transparent" allowFullScreen="true" allowScriptAccess="always" name="Metacafe_#{metacafe_id}" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed>}
     return result
   end
 }
